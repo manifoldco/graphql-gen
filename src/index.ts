edbd995eb5bff93ac6e622b812e5586bd8314551
Drew Powers <drew@pow.rs>
2019-01-30 14:17:41 -0700

    Add test for entry

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import swagger2, { Swagger2 } from './swagger-2';
+import swagger2gen, { Swagger2 } from './swagger-2';
 
-export default (spec: Swagger2, options?: { output: string; swagger?: number }) => {
+export default function(spec: Swagger2, options?: { output?: string; swagger?: number }) {
   const swagger = (options && options.swagger) || 2;
 
-  if (swagger === 1 || swagger === 3) {
-    console.error('That version is not supported');
-    return;
+  if (swagger !== 2) {
+    throw new Error(`Swagger version ${swagger} is not supported`);
   }
 
-  return swagger2(spec);
-};
+  return swagger2gen(spec);
+}
